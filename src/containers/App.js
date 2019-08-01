e618435e1d731040259afb68cf32e80a97d55fff
Ryan Dulac <analyticrye@gmail.com>
2019-08-01 01:45:35 -0400

    added scroll component and cleaned up code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 
-import CardList from "./cardList";
+import CardList from "../components/cardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/scroll";
 
-import SearchBox from "./SearchBox";
 import "./App.css";
 
 class App extends Component {
@@ -24,22 +25,22 @@ class App extends Component {
     this.setState({ searchfield: e.target.value });
   }
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (this.state.robots.length === 0) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+
+    return !robots.length ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
           <CardList robots={filteredRobots} />
-        </div>
-      );
-    }
+        </Scroll>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-import App from "./App";
+import App from "./containers/App";
 
 import "./index.css";
 import "tachyons";
